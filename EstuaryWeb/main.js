f42e3b9c6ab61e690ee37f28174c3cf56809cd0e
huylongdoBrave <huylong.do@estuary.solutions>
2025-10-12 22:34:58 +0700

    12.10

--- EstuaryWeb/main.js
+++ EstuaryWeb/main.js
@@ -6,22 +6,55 @@
 //     });
 // });
 
-    let currentIndex = 0;
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
 
-    function moveSlide(direction) {
-        const slides = document.querySelector('.slides');
-        const totalSlides = document.querySelectorAll('.slide').length;
+// Menu responsive
+const mobileMenu = document.getElementById('mobile-menu');
+const menu = document.getElementById('menu');
 
-        currentIndex += direction;
+mobileMenu.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
 
-        // Kiểm tra biên
-        if (currentIndex < 0) {
-            currentIndex = totalSlides - 1;
-        } else if (currentIndex >= totalSlides) {
-            currentIndex = 0;
-        }
 
-        // Di chuyển slider
-        const offset = -currentIndex * 100; // Tính toán độ dịch chuyển
-        slides.style.transform = `translateX(${offset}%)`;
+// Feature search
+function search() {
+    const query = document.getElementById('search-input').value;
+    alert('Bạn đã tìm kiếm: ' + query);
+    // Thay thế alert bằng logic tìm kiếm thực tế của bạn
+}
+
+//  Enter event search
+document.getElementById('search-input').addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        search();
     }
+});
+
+// feature slider
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+
+    currentIndex += direction;
+
+    // Kiểm tra biên
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    // Di chuyển slider
+    const offset = -currentIndex * 100; // Tính toán độ dịch chuyển
+    slides.style.transform = `translateX(${offset}%)`;
+}
